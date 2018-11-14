d011e99cebfdcf3c1a25f06d36d8cea79cb18fc3
Jeroen <druwe.jeroen@gmail.com>
2018-11-14 21:55:34 +0100

    Testing out some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  name: string;
+  checkedIn: boolean;
+  state: string | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,8 +13,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
+  showContent: boolean;
+  persons: Person[] = [
+    {
+      checkedIn: true,
+      name: 'Jeroen',
+      state: null
+    },
+    {
+      checkedIn: false,
+      name: 'Bob',
+      state: 'yey'
+    }
+  ];
 
   constructor() {
     this.title = 'Jeroen';
+  }
+
+  handleButtonClick(value: string) {
+    alert('Button clicked! ' + value);
+    this.showContent = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ReversePipe } from './reverse.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
